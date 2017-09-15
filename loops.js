43c9fac97161e1411547a10dfdd08c85cd8ed9df
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-15 22:00:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,8 @@ function forLoop (array) {
 }
 
 function whileLoop(n){
-  let countdown = 100
-  while (countdown > 0){
-    console.log(--countdown)
+  while (n > 0){
+    console.log(--n)
   }
   return "done"
 }
